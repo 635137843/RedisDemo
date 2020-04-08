e2b1c065fc61ec6d2ac1ed11098f5b6a7bfd0631
mingxin he <635137843@qq.com>
2020-04-09 01:01:27 +0800

    使用自己的工具类

--- src/test/java/com/hmx/redistest/TestApplicationTests.java
+++ src/test/java/com/hmx/redistest/TestApplicationTests.java
@@ -3,8 +3,10 @@ package com.hmx.redistest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hmx.redistest.pojo.User;
+import com.hmx.redistest.utils.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -12,7 +14,17 @@ import org.springframework.data.redis.core.RedisTemplate;
 class TestApplicationTests {
 
     @Autowired
+    @Qualifier("redisTemplate")//使用自己封装的模板
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Test
+    public void test(){
+        redisUtil.set("name","hmx");
+        System.out.println(redisUtil.get("name"));
+    }
 
     @Test
     void contextLoads() throws JsonProcessingException {
